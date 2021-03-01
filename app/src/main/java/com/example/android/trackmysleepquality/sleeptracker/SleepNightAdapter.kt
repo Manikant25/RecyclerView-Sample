3dd28bdd05683678fb87890fe176c454839689a0
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-02 02:47:24 +0530

    Created viewHolder for all the sleep data

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -2,40 +2,60 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
-    var data =  listOf<SleepNight>()
+class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
+    var data = listOf<SleepNight>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-//    when the RecyclerView needs a view holder.
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-    val layoutInflater = LayoutInflater.from(parent.context)
-    val view = layoutInflater
-            .inflate(R.layout.text_item_view, parent, false) as TextView
-    return TextItemViewHolder(view)
+    //    when the RecyclerView needs a view holder.
+    override fun onCreateViewHolder(
+            parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater =
+                LayoutInflater.from(parent.context)
+        val view = layoutInflater
+                .inflate(R.layout.list_item_sleep_night,
+                        parent, false)
+        return ViewHolder(view)
     }
 
-//    RecyclerView to display the data for one list item at the specified position
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-    val item = data[position]
-    if (item.sleepQuality <= 1) {
-        holder.textView.setTextColor(Color.RED) // red
-    }
-    holder.textView.text = item.sleepQuality.toString()
+    //    RecyclerView to display the data for one list item at the specified position
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+        val res = holder.itemView.context.resources
+        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+
+        holder.qualityImage.setImageResource(when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
     }
 
-//    RecyclerView needs to know how many items the adapter has for it to display
+    //    RecyclerView needs to know how many items the adapter has for it to display
     override fun getItemCount() = data.size
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+        val quality: TextView = itemView.findViewById(R.id.quality_string)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+    }
 
 
 }
